c36643fa0f1cc27ca11f11a54a745a6bf190faa9
adelademarku <adelademarku@email.com>
2022-11-14 20:24:23 +0100

    jsnack5

---
+++ jSnack5/script.js
@@ -0,0 +1,23 @@
+let arrayNuova = [];
+
+
+
+for (let i=0; i<6; i++) {let numeriInseriti = prompt("Inserisci un numero") ;
+
+if (numeriInseriti %2== 1){ 
+arrayNuova.push(numeriInseriti);} 
+
+ /*else if (numeriInseriti % 2 == 0) { console.log ("numero dispari");} */
+
+}
+
+console.log (arrayNuova);  
+
+
+
+
+
+
+ 
+
+

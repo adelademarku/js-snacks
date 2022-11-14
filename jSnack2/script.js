84d4ccc24df3e0f86db249ba268080b5a7f64ef0
adelademarku <adelademarku@email.com>
2022-11-14 16:09:40 +0100

    sistemazione js2

--- jSnack2/script.js
+++ jSnack2/script.js
@@ -1,28 +1,22 @@
 let primaParola = prompt("Inserisci una parola");
-let secondaParola = prompt ("Inserisci un'altra parola");
+let secondaParola = prompt("Inserisci un'altra parola");
 
 //stabilisco la lunghezza delle parola
 
 
 
-let lenprimaParola = primaParola.length;
-let lensecondaParola =secondaParola.length;
 
-console.log ("la lunghezza della prima parola è: "+ lenprimaParola);
-console.log ("la lunghezza della seconda parola è: "+ lensecondaParola);
-
-//stabilisco la parola piu lunga e corta
+if (primaParola > secondaParola) {
+    console.log("la prima parola è piu lunga:" + primaParola);
+} else if (secondaParola > primaParola) {
+    console.log("la seconda parola è piu lunga : " + secondaParola); }
 
 
-let parolaPiuLunga = Math.max (lenprimaParola,lensecondaParola);
-let parolaPiuCorta = Math.min (lenprimaParola,lensecondaParola); 
+if (primaParola < secondaParola) {
+    console.log("la prima parola è piu corta:" + primaParola);  
+} else if (secondaParola > primaParola) {
+    console.log("la seconda parola è piu corta: " + secondaParola);
+} 
 
-
-//stampo le parole 
-
-console.log ("la parola piu lunga è: "+ parolaPiuLunga);
-console.log ("la parola piu corta è: "+ parolaPiuCorta);  
-
-
-// bonus: se le parole sono uguali 
-if (lenprimaParola == lensecondaParola){console.log("la lunghezza è uguale.");} 
+if (primaParola == secondaParola) { 
+    console.log("la lunghezza è uguale."); }

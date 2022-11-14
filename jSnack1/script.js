413072592e40476cac27415a3b9c2ccac8cd8738
adelademarku <adelademarku@email.com>
2022-11-14 14:35:00 +0100

    jssnack1

---
+++ jSnack1/script.js
@@ -0,0 +1,11 @@
+//chiedo di inserire due numeri
+let primoNumero = prompt ("Inserisci un numero")
+let secondoNumero = prompt ("Inserisci un altro'numero")
+
+//vedo quale numero è piu grande
+
+let numeroMaggiore = Math.max(primoNumero,secondoNumero);
+
+//stampo in console
+
+console.log("il numero maggiore è: " + numeroMaggiore)

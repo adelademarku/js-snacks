9f0ab0d73a2a9e21783f2a4198ffa4afb3f99b23
adelademarku <adelademarku@email.com>
2022-11-14 19:09:16 +0100

    jsnack3

---
+++ jSnack3/script.js
@@ -0,0 +1,42 @@
+
+let primonumero = prompt("Inserisci un numero");
+let secondonumero = prompt("Inserisci un numero");
+let terzonumero = prompt("Inserisci un numero");
+let quartonumero = prompt("Inserisci un numero");
+let quintonumero = prompt("Inserisci un numero");
+let sestonumero = prompt("Inserisci un numero");
+let settimonumero = prompt("Inserisci un numero");
+let ottavonumero = prompt("Inserisci un numero");
+let nononumero = prompt("Inserisci un numero");
+let decimonumero = prompt("Inserisci un numero");
+
+
+let num1 = parseInt (primonumero);
+let num2 = parseInt (secondonumero);
+let num3 = parseInt (terzonumero);
+let num4 = parseInt (quartonumero);
+let num5 = parseInt (quintonumero);
+let num6 = parseInt (sestonumero);
+let num7 = parseInt (settimonumero);
+let num8 = parseInt (ottavonumero);
+let num9 = parseInt (nononumero);
+let num10 = parseInt (decimonumero); 
+
+let somma = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10 ; 
+
+console.log('The sum of ' + num1 + ' and ' + num2 +   ' and ' + num3 +'and' + num4 +'and' + num5 +' and ' + num6 +' and ' + num7 +
+ ' and ' + num8 +' and ' + num9 +' and ' + num10 + ' is: ' + somma);
+
+
+/*
+ 
+for (let i=0; i<10; i++) {let numeroRichiesto = prompt("Inserisci un numero") ; numeroRichiesto= parseInt(numeroRichiesto) 
+
+
+
+
+} */
+
+
+
+
